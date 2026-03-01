801978e8bf904e0212884b631a1b086bb872221e
Shiver211 <2863677073@qq.com>
2026-03-01 10:32:47 +0800

    仅能标记一个物品

--- src/main/java/dev/beecube31/crazyae2/common/containers/ContainerMechanicalBotaniaTileBase.java
+++ src/main/java/dev/beecube31/crazyae2/common/containers/ContainerMechanicalBotaniaTileBase.java
@@ -4,10 +4,15 @@ import appeng.api.config.*;
 import appeng.api.networking.IGridHost;
 import appeng.api.networking.IGridNode;
 import appeng.api.util.AEPartLocation;
+import appeng.helpers.InventoryAction;
 import appeng.helpers.IContainerCraftingPacket;
+import dev.beecube31.crazyae2.common.containers.base.slot.SlotFake;
 import dev.beecube31.crazyae2.common.enums.BotaniaMechanicalDeviceType;
 import dev.beecube31.crazyae2.common.tile.botania.TileBotaniaMechanicalMachineBase;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 
 public abstract class ContainerMechanicalBotaniaTileBase extends ContainerCrazyAEUpgradeable implements IContainerCraftingPacket {
 
@@ -67,5 +72,23 @@ public abstract class ContainerMechanicalBotaniaTileBase extends ContainerCrazyA
         this.verifyPermissions(SecurityPermissions.BUILD, false);
 
         this.standardDetectAndSendChanges();
+    }
+
+    @Override
+    public void doAction(final EntityPlayerMP player, final InventoryAction action, final int slot, final long id) {
+        if (action == InventoryAction.PICKUP_OR_SET_DOWN && slot >= 0 && slot < this.inventorySlots.size()) {
+            final Slot targetSlot = this.getSlot(slot);
+            if (targetSlot instanceof SlotFake) {
+                final ItemStack hand = player.inventory.getItemStack();
+                if (!hand.isEmpty()) {
+                    final ItemStack configuredStack = hand.copy();
+                    configuredStack.setCount(1);
+                    targetSlot.putStack(configuredStack);
+                    return;
+                }
+            }
+        }
+
+        super.doAction(player, action, slot, id);
     }
 }
